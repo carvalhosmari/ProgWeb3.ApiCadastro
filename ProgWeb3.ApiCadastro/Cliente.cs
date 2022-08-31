4f3062914372eb78a46a11d2d639f8368f1e0ccc
carvalhosmari <mscarvall@gmail.com>
2022-08-31 17:05:42 -0300

    inclusão de decorators, statuscodes e dataAnnotations para validações.

--- ProgWeb3.ApiCadastro/Cliente.cs
+++ ProgWeb3.ApiCadastro/Cliente.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProgWeb3.ApiCadastro
 {
     public class Cliente
     {
-        public string? Nome { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório!")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Cpf é obrigatório!")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O Cpf deve conter 11 dígitos!")]
         public string Cpf { get; set; }
+
+
+        [Required(ErrorMessage = "Data é obrigatória!")]
         public DateTime DataNasc { get; set; }
+
+
         public int Idade => CalculaIdade();
 
 
--- ProgWeb3.ApiCadastro/Controllers/ClienteController.cs
+++ ProgWeb3.ApiCadastro/Controllers/ClienteController.cs
@@ -4,6 +4,8 @@ namespace ProgWeb3.ApiCadastro.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class ClienteController : ControllerBase
     {
         private static readonly string[] Nomes = new[]
@@ -12,7 +14,7 @@ namespace ProgWeb3.ApiCadastro.Controllers
         };
         private static readonly string[] Cpfs = new[]
         {
-        "502.788.640-31", "614.199.180-79", "078.675.490-72", "567.742.100-66", "496.796.130-46", "649.273.340-15", "584.377.670-08", "728.479.940-41", "487.565.200-34", "011.596.380-47"
+        "50278864031", "61419918079", "07867549072", "56774210066", "49679613046", "64927334015", "58437767008", "72847994041", "48756520034", "01159638047"
         };
 
         private readonly ILogger<ClienteController> _logger;
@@ -29,34 +31,75 @@ namespace ProgWeb3.ApiCadastro.Controllers
                 Cpf = Cpfs[Random.Shared.Next(Cpfs.Length)],
                 DataNasc = DateTime.Now.AddDays(-Random.Shared.Next(6450, 38349))
             }).ToList();
-            
+
         }
 
-        [HttpGet]
-        public List<Cliente> Get()
+        [HttpGet("/clientes/consultar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Cliente>> Get()
         {
-            return Clientes;
+            return Ok(Clientes);
         }
 
-        [HttpPost]
-        public Cliente Insert(Cliente novoCliente)
+        [HttpGet("/cliente/{cpf}/consultar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<Cliente>> Get2(string cpf)
+        {
+            var cpfCadastrado = Clientes.Find(Clientes => Clientes.Cpf == cpf);
+
+            if (cpfCadastrado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cpfCadastrado);
+        }
+
+        [HttpPost("/cliente/inserir")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<Cliente> Insert([FromBody] Cliente novoCliente)
         {
             Clientes.Add(novoCliente);
-            return novoCliente;
+            return CreatedAtAction(nameof(Get), novoCliente);
         }
 
-        [HttpPut] 
-        public Cliente Update(int index, Cliente cliente)
+        [HttpPut("/cliente/{cpf}/atualizar")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Cliente> Update(string cpf, Cliente cliente)
         {
-            Clientes[index] = cliente;
-            return Clientes[index];
+            var cpfCadastrado = Clientes.Find(Clientes => Clientes.Cpf == cpf);
+
+            if (cpfCadastrado == null)
+            {
+                return NotFound();
+            }
+
+            cpfCadastrado.Nome = cliente.Nome;
+            cpfCadastrado.Cpf = cliente.Cpf;
+            cpfCadastrado.DataNasc = cliente.DataNasc;
+
+            return Ok(cpfCadastrado);
         }
 
-        [HttpDelete]
-        public List<Cliente> Delete(int index)
+        [HttpDelete("/cliente/{cpf}/deletar")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(string cpf)
         {
-            Clientes.RemoveAt(index);
-            return Clientes;
+            var cpfCadastrado = Clientes.Find(Clientes => Clientes.Cpf == cpf);
+
+            if (cpfCadastrado == null)
+            {
+                return NotFound();
+            }
+
+            Clientes.Remove(cpfCadastrado);
+
+            return NoContent();
         }
     }
 }
